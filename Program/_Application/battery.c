ddfde682a387bd629ff095ce16be00e9e1a77acd
emil.wojtaszek <emil.wojtaszek@gmail.com>
2013-02-06 20:05:49 +0100

    change gitignore

---
+++ Program/_Application/battery.c
@@ -0,0 +1,4 @@
+#include "battery.h"
+
+volatile char adc_string[80];
+volatile unsigned int buforADC[8];
---
+++ Program/_Application/battery.h
@@ -0,0 +1,10 @@
+
+#ifndef BATTERY_H
+#define BATTERY_H
+
+// keep measured temperatures from 6 sensors
+extern volatile char adc_string[80];
+extern volatile unsigned int buforADC[8];
+
+#endif // BATTERY_H
+
